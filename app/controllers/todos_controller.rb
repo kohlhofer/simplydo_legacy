4763e60afebf82a724daaa376aad94fe44fa74f5
Daniel Steiner <steini@Steini.(none)>
2009-02-07 17:08:44 +0100

    todo display

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,22 +11,27 @@ class TodosController < ApplicationController
   def show
     @todo_lists = TodoList.find(:all, :order => 'name ASC')
     @todo_list = @todo_lists.first
-    @todos = Todo.find_by_list(@todo_list)
-    @todo = Todo.new
+    @open_todos = Todo.find_by_list(@todo_list)
+    @closed_todos = Todo.find_by_list(@closed_list)
+
+    @todo = Todo.new(:todo_list => @todo_list)
+    #@todo.list = @todo_list.id
   end
 
   def list
     @todo_lists = TodoList.find(:all, :order => 'name ASC')
     @todo_list = @todo_lists.reject() { |todo_list| todo_list.id != params[:todo_list].to_i}.first
-    @todos = Todo.find_by_list(params[:list])
-    @todo = Todo.new
-    
+    @open_todos = Todo.find_by_list(params[:todo_list])
+    @closed_todos = Todo.find_by_list(params[:todo_list], 1)
+    @todo = Todo.new(:todo_list => @todo_list)
+
     render :action => 'show'
   end
 
   def create
     @todo = Todo.new(params[:todo])
-
+    @todo.done = 0
+    
     unless @todo.save!
       
     end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,7 +1,9 @@
 class Todo < ActiveRecord::Base
 
-  def self.find_by_list(todo_list)
-    []
+  belongs_to :todo_list
+
+  def self.find_by_list(todo_list, done = 0)
+    Todo.find(:all, :conditions => ['todo_list_id = ? AND done = ?', todo_list, done])
   end
 
 end
