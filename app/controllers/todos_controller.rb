e0a37c1dea517c6b434fa1fd73649d4e1742d505
Daniel Steiner <steini@Steini.(none)>
2009-02-07 16:29:29 +0100

    add todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,9 @@
 require 'app/controllers/application.rb'
 
 class TodosController < ApplicationController
-  
+
+  layout 'application', :except => ['create']
+
   def index
     show
   end
@@ -10,17 +12,24 @@ class TodosController < ApplicationController
     @todo_lists = TodoList.find(:all, :order => 'name ASC')
     @todo_list = @todo_lists.first
     @todos = Todo.find_by_list(@todo_list)
+    @todo = Todo.new
   end
 
   def list
-
     @todo_lists = TodoList.find(:all, :order => 'name ASC')
-
     @todo_list = @todo_lists.reject() { |todo_list| todo_list.id != params[:todo_list].to_i}.first
-
     @todos = Todo.find_by_list(params[:list])
-
+    @todo = Todo.new
+    
     render :action => 'show'
+  end
+
+  def create
+    @todo = Todo.new(params[:todo])
+
+    unless @todo.save!
+      
+    end
   end
 
 end
