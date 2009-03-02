39cb16b0027afb1ea51df24ccbb7397ec02a6073
steini <steiniman@gmail.com>
2009-03-02 02:06:18 +0100

    changed exception mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,5 +27,7 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-ExceptionNotifier.exception_recipients = %w(steiniman@gmail.com)
-ExceptionNotifier.email_prefix = "[simplydo] "
+config.after_initialize do
+  ExceptionNotifier.exception_recipients = %w(steiniman@gmail.com)
+  ExceptionNotifier.email_prefix = "[simplydo] "
+end
