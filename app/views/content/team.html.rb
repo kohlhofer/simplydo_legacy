c5771644c4cf063fc7bce544aa5985a96fd889a5
steini <steiniman@gmail.com>
2009-03-02 01:57:36 +0100

    deleted old file, added active_record session store

--- app/views/content/team.html.rb
+++
@@ -1,7 +0,0 @@
-<div id="team" class="page">
-  <h1><a href="/">SimplyDo.com</a> is brought to you by ...</h1>
-  <% for team_member in %w(daniel alex).sort_by { rand } %>
-    <%= render :partial => "content/#{team_member}" %>
-  <% end %>
-  <div class="clearThis"> </div>
-</div>
